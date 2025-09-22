57ac5eb1dddf608cf320d8d786d715e71d19f9c4
rubbal123 <rubbalblockcoders@gmail.com>
2025-09-22 18:50:37 +0530

    added config for globlal env config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    //generate password hash
-    const hashedPassword = await argon.hash(dto.password);
+    try {
+      //generate password hash
+      const hashedPassword = await argon.hash(dto.password);
 
-    //save user in db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hashedPassword,
-      },
-    });
+      //save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hashedPassword,
+        },
+      });
 
-    const { hash, ...userWithoutHash } = user;
-    //return new user
-    return userWithoutHash;
+      const { hash, ...userWithoutHash } = user;
+      //return new user
+      return userWithoutHash;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('user already exists');
+        }
+      }
+      throw error;
+    }
   }
 
-  login() {
-    return 'I am signed in';
+  async signin(dto: AuthDto) {
+    try {
+      //find user
+      const user = await this.prisma.user.findUnique({
+        where: { email: dto.email },
+      });
+      if (!user) {
+        throw new ForbiddenException('Invalid Credentials');
+      }
+      const pwMatches = await argon.verify(user.hash, dto.password);
+
+      if (!pwMatches) {
+        throw new ForbiddenException('Invalid Credentials');
+      }
+      const { hash, ...userWithoutHash } = user;
+      return userWithoutHash;
+    } catch (error) {
+      throw error;
+    }
   }
 }
